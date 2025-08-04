49f27563e1ea8beab71211c0f940c751f443ce15
ilknur99926 <elbrusqizi@gmail.com>
2025-08-04 19:32:24 +0400

    yeni backend

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Bitter Soul Backend işləyir ☕');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server http://localhost:${PORT} ünvanında işləyir`);
+});
